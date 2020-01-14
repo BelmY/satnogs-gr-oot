12cfc33cf43a2b9cf93b36204de227894be5b8ed
Manolis Surligas <surligas@gmail.com>
2020-01-14 20:30:07 +0200

    Integrate a variation of the Constellation Modulator
    The GNU Radio Constellation Modulator block uses
    the polyphase arbitrary resampler and RRC filter
    taps to perform pulse shaping.
    
    This introduces an offset to the tag propagated throughout
    the stream. To deal with this issue, a small variation of
    this block has been integrated into the gr-satnogs codebase.
    Our custom block declares the sample delay of the RRC filter
    to the GNU Radio scheduler, so the tags can be propagated
    properly towards other blocks, eg the burst shaper.

--- python/__init__.py
+++ python/__init__.py
@@ -16,6 +16,25 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
+# gr-satnogs: SatNOGS GNU Radio Out-Of-Tree Module
+#
+#  Copyright (C) 2020
+#  Libre Space Foundation <http://libre.space>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+
 # The presence of this file turns this directory into a Python package
 
 '''
@@ -30,6 +49,7 @@ try:
     # this might fail if the module is python-only
     from .satnogs_swig import *
     from .doppler_compensation import doppler_compensation
+    from .generic_mod import generic_mod
     from .utils import *
 except ImportError as err:
     sys.stderr.write("Failed to import SatNOGS ({})\n".format(err))
